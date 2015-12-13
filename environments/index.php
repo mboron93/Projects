678a9e8418bc362a07d0270c1c68c89f000922d0
Marcin Groth <krasny1193@gmail.com>
2015-12-13 17:12:30 +0100

    strona startowa poczatek

--- environments/index.php
+++ environments/index.php
@@ -36,6 +36,7 @@ return [
             'backend/web/assets',
             'frontend/runtime',
             'frontend/web/assets',
+            'frontend/web',
         ],
         'setExecutable' => [
             'yii',
--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -111,8 +111,8 @@ class SiteController extends Controller
     public function actionLogout()
     {
         Yii::$app->user->logout();
-
         return $this->goHome();
+
     }
 
     /**
@@ -191,7 +191,21 @@ class SiteController extends Controller
             'model' => $model,
         ]);
     }
+        
+    public function actionForm(){
+        if(Yii::$app->request->post())
+        {
+           
+          
+          // var_dump($_GET);
+            $plik = $_FILES['obr']['tmp_name'];
+            $nazwa =  md5(uniqid(time())).'jpg';
+          move_uploaded_file($plik,Yii::$app->basePath.'/web/wasd'.$nazwa);
+                var_dump($_POST);
 
+        }
+        return $this->vendor('index');
+    }
     /**
      * Resets password.
      *
@@ -216,5 +230,6 @@ class SiteController extends Controller
         return $this->render('resetPassword', [
             'model' => $model,
         ]);
+        
     }
 }
