7abcc9c57f6ecb04d4b0226a941ab95bd951443c
xpowerpl <xpowerpl@wp.pl>
2016-01-10 20:02:55 +0100

    Wstępna zmiana wyglądu panelu administracyjnego

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -7,27 +7,26 @@ $this->title = 'My Yii Application';
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
-			 
-                    <a href="/city">	
-                    <button type="button" class="btn btn-info btn-block btn-lg">
-				City
-			</button> 
-                    </a>
-                    <a href="/restaurant">	
-			<button type="button" class="btn btn-info btn-lg btn-block">
-				Restaurant
-			</button> 
-                    </a>
-                    <a href="/dish">	
-			<button type="button" class="btn btn-info btn-lg btn-block">
-				Dish
-			</button> 
-                    </a>
-                    <a href="/order">
-			<button type="button" class="btn btn-info btn-lg btn-block">
-				Order
-			</button>
-                    </a>
+			<div class="list-group">
+				 <a href="#" class="list-group-item active">Panel Administracyjny</a>
+				<div class="list-group-item">
+					<h4 class="list-group-item">
+                                            <a href="/city">Dodaj / Usuń Miasto</a><span class="badge">+/-</span>
+					</h4>
+                                        <h4 class="list-group-item">
+                                            <a href="/restaurant">Dodaj / Usuń Restaurację</a><span class="badge">+/-</span>
+					</h4>
+                                        <h4 class="list-group-item">
+                                            <a href="/dish">Dodaj / Usuń Danie</a><span class="badge">+/-</span>
+					</h4>
+                                        <h4 class="list-group-item">
+                                            <a href="/order">Dodaj / Usuń Zamówienie</a><span class="badge">+/-</span>
+					</h4>
+				</div>
+				<div class="list-group-item">
+   <!--trzeba zrobić prawidłowe odwołanie do site pomoc.php-->    <a href=#pomoc.php>Help</a><span class="badge">?</span>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
---
+++ backend/views/site/pomoc.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $name string */
+/* @var $message string */
+/* @var $exception Exception */
+
+use yii\helpers\Html;
+
+$this->title = 'pomoc';
+?>
+<div class="site-error">
+
+    <p>
+        jakiś tam poradnik
+    </p>
+</div>
