d5e13a383b43904c9d7dc68ed3c8d023792a60ef
marcin Groth <krasny1193@gmail.com>
2016-03-06 18:57:22 +0100

    dodane dane restauracji zrobcie php yii migrate

---
+++ console/migrations/m160306_175527_danerestauracji.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+class m160306_175527_danerestauracji extends Migration
+{
+    public function up()
+    {
+              $this->execute("  ALTER TABLE  `restaurant` ADD  `telefon` INT( 11 ) NOT NULL ,
+ADD  `e-mail` VARCHAR( 55 ) NOT NULL ;");
+
+    }
+
+    public function down()
+    {
+        echo "m160306_175527_danerestauracji cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
